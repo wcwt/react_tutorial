02ba706ad2e3c7feefea1edaf830432ddd33378d
wcwt <51042202+wcwt@users.noreply.github.com>
2020-12-30 12:34:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+ReactDom.readar(
+    //What I want
+    <h1>Hello WOrld</h1>,
+    //where I want
+    document.getElementById("root")
+)
