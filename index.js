8ceb7f33831510f5730463c05ff62c9b521e47bc
wcwt <51042202+wcwt@users.noreply.github.com>
2020-12-30 12:42:23 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 import React from "react"
 import ReactDom from "react-dom"
 
-ReactDom.readar(
-    //What I want
-    <h1>Hello WOrld</h1>,
-    //where I want
-    document.getElementById("root")
-)
+ReactDom.reader(<h1>Hello WOrld</h1>,document.getElementById("root") )
