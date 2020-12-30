f523f680d811969e7c63df7ae85cd2a7593b87d3
wcwt <51042202+wcwt@users.noreply.github.com>
2020-12-30 18:10:46 +0800

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,26 +1,23 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+function Joke(props){
+   return(
+      <div>
+         <p>Question is {props.question}</p>
+         <p>Answer is {props.answer}</p>
+      </div>
+   )
+}
 
 function App(){
-   const date = new Date()
-   const hour = date.getHours()
-   let message
-
-   if (hour < 12)
-      message = "morning"
-   else if (hour >=12 && hour <= 17)
-      message = "afternoon"
-   else  
-      message = "night"
-   
-   const h1_style = {
-      color: "rgb(121, 33, 194)",
-      backgroundColor:"rgb(190, 133, 91)" ,
-      fontSize: 20
-   }
-   return (
-      <h1 style= {h1_style}>Good {message}</h1>
+   return(
+      <div>
+         <Joke 
+            question = "123" 
+            answer = "456"
+         />       
+      </div>
    )
 }
 
